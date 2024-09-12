f9ffd6a5a726dfa42d7221f99e8a8d62b2d4357d
Samuel <sseo001@gmail.com>
2024-09-12 09:37:48 +0900

    Add files via upload

---
+++ 0_🏡_SoolyTech_Home.py
@@ -0,0 +1,69 @@
+# soolytech landing pages
+
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="수리테크",
+    page_icon=":house_with_garden:",
+)
+
+st.header(":house_with_garden: Welcome to SoolyTech ")
+st.divider()
+
+st.markdown("#### 수리(數理) 테크는 수학 이론에 기반한 IT 기술에서 시작합니다")
+
+st.markdown("> :speech_balloon: 수학의 확률, 해석학 및 최적화 분야 이론을 금융 및 AI 분야에 적용합니다"
+)
+
+st.divider()
+
+st.markdown("#### :one: 투자자문")
+
+st.markdown('''
+>> :speech_balloon: 금융공학을 통해 상승 시 수익을 일부 포기하는 대신 하락의 위험을 헤지하는 ELS 복제 전략
+
+>>> ELS 복제 전략은 변동성 및 시장상황에 따라서 손실이 주기적으로 크게 발생하는 경향이 있고   
+>>> 이런 경우를 방지하는 시장 중립 전략과의 연계가 필요
+
+>>> Buffer ETF 상품 개발
+
+>> :speech_balloon: 투자자문을 통한 랩 어카운트 및 사모펀드 형식으로 판매됨
+
+>>> 퇴직연금을 위한 로보어드바이저 형식의 전략으로 개선
+''' )
+
+
+st.divider()
+
+st.markdown("#### :two: 금융 특화 AI 활용")
+
+st.markdown('''
+>> :speech_balloon: AI의 거짓정보(Hallucination)을 방지하는 기술
+
+>>> 금융 정보에 특화한 fine tuning    
+>>> Knowledge graph, Semantic reasoning, Knowledge acquisition
+
+>> :speech_balloon: 개인 금융정보 조회 서비스인 Open API와 연계
+
+>>> 모든 금융사에 보유한 금융상품에 대한 정보를 확인하고 분석
+''' )
+
+st.divider()
+
+st.markdown("#### :three: AI 솔루션")
+
+st.markdown('''
+>> :speech_balloon: Intelligent Tutoring
+
+>>> Machine Learing 기반 교육 서비스       
+>>> 자격증 시험 등 목표 달성을 위한 학습 서비스에 적합
+
+>> :speech_balloon: On premise AI
+
+>>> 기업 요구에 맞춤으로 AI 솔루션 제공    
+>>> Knowledge Acquisition 방법론을 통한 fine tuning 기술
+''' )
+
+st.divider()
+
---
+++ pages/1_📈_Investment_Strategy.py
@@ -0,0 +1,115 @@
+# page1 : 투자자문 서비스
+
+import streamlit as st
+import pandas as pd
+import altair as alt
+from math import sqrt
+import sqlite3
+import plotly.graph_objects as go
+
+
+st.markdown("#### :one: 투자자문")
+
+# soft 줄바꿈 하려면 space를 넣어야함. 아래 첫줄 끝에 공백 필요
+st.markdown('''
+- 주가가 일정 수준 하락해도 수익을 보는 Buffer 전략      
+>> 해외에서 구조화 상품을 ETF로 변환한 Buffer ETF 인기있음    
+
+- 긴 투자기간 동안 안정적인 수익추구: 퇴직연금에 적합
+
+- ELS 복제 전략을 개선한 Buffer 전략     
+>> 운용 및 마케팅 지원 시스템으로 업무 효율화
+''' )
+
+st.divider()
+
+items = ['SK하이닉스','삼성전자','현대차','기아','KB금융','NAVER', '삼성생명', '셀트리온']
+
+
+st.markdown("##### :a: Market watch (예시)")
+############################# 종목 선택 (입력)
+item_name = st.selectbox("종목을 선택하세요", items)
+
+conn= sqlite3.connect('soolyDB.db')
+
+sql_query = pd.read_sql_query(
+    ('select * from hist_data where name = :value'),
+    conn,
+    params={'value': item_name}
+)
+
+############################### chart data preparing
+df = pd.DataFrame(sql_query)
+
+chart_data = pd.DataFrame(df.head(210), columns=["date", "open", "high", "low", "close"])
+chart_data['date'] = pd.to_datetime(chart_data['date'], format="%Y%m%d")
+
+############################# 변동성 구하기
+chart_data['daily_return'] = chart_data['close'].pct_change()
+chart_data['7d_vol'] = chart_data['daily_return'].rolling(window=7).std() * sqrt(252)
+chart_data['15d_vol'] = chart_data['daily_return'].rolling(window=15).std() * sqrt(252)
+chart_data['30d_vol'] = chart_data['daily_return'].rolling(window=30).std() * sqrt(252)
+chart_data['60d_vol'] = chart_data['daily_return'].rolling(window=60).std() * sqrt(252)
+
+vol_data = pd.DataFrame(chart_data.head(150), columns=["date", "7d_vol", "15d_vol", "30d_vol", "60d_vol"])
+
+#####################################
+subtitle = str(item_name) + " 주가 그래프"
+st.markdown(subtitle)
+
+domain_max = max(chart_data['close']) * 1.1
+domain_min = min(chart_data['close']) * 0.9
+
+chart = alt.Chart(chart_data).mark_line().encode(
+    x='date',
+    y=alt.Y('close', scale=alt.Scale(domain=[domain_min,domain_max]))
+)
+
+st.altair_chart(chart, use_container_width=True)
+
+################################
+subtitle = str(item_name) + " 캔들스틱 차트"
+st.markdown(subtitle)
+
+fig = go.Figure()
+fig.add_trace(go.Candlestick(x=chart_data['date'], open=chart_data['open'], high=chart_data['high'], low = chart_data['low'], close=chart_data['close']))
+
+st.plotly_chart(fig)
+
+################################
+subtitle = str(item_name) + " 변동성 그래프 (검증 중)"
+st.markdown(subtitle)
+
+chart = alt.Chart(vol_data).transform_fold(['7d_vol', '15d_vol', '30d_vol', '60d_vol']).mark_line().encode(
+    x='date',
+    y='value:Q',
+    color='key:N'
+)
+
+st.altair_chart(chart, use_container_width=True)
+st.divider()
+##############################
+st.markdown("Main Book 정보")
+bookinfo ={
+    '상품코드': ['2024001', '2024002', '2024003'],
+    '발행일' :['2023-10-5', '2023-11-5', '2024-05-02'],
+    '발행금액' : ['100,000,000', '250,000,000', '120,000,000'],
+    '기초자산' : ['KOSPI200', '삼성전자', 'SK하이닉스'],
+    '목표수익률' : ['6%', '7%', '10%'],
+    '상품명' : ['Buffer 1호', 'Buffer 2호', 'Buffer 3호'],
+    'NAV' : ['101.4', '103.3', '105.2']
+}
+df = pd.DataFrame(bookinfo)
+
+st.dataframe(df)
+st.divider()
+################# 상품 설명서
+
+if st.button("상품설명서 생성"):
+    st.image('samsungELS.png', caption='삼성증권 ELS 상품 예시')
+
+st.button("Reset(다시시작)", type="primary")
+st.divider()
+################# working on
+
+st.markdown('백테스팅, 운용관리 등 working on ... :sun_with_face:')
